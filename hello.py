1fd77dd5a9deb7e62ff492c27896558ea3d0730d
Alena# <marahaya@gmail.com#>
2013-11-02 13:15:40 -0400

    Change to request variable

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('hello.html', person="Alena")
+    return render_template('hello.html', person=request.args.get('person'))
 
 if __name__ == "__main__":
     app.run()
