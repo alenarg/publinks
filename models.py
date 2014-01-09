7e5267266d9e822db12d5eec292581f5a61f704a
Alena# <marahaya@gmail.com>
2014-01-09 09:23:40 -0500

    Models in progress

---
+++ models.py
@@ -0,0 +1,16 @@
+
+
+workshops = [
+    {
+        "title": "Start Something, General",
+        "date": datetime.date(2014, 1, 14),
+        "google_docs_form_id": "1zOb70KGf3R6X5vozKzX_Md3ZMQt5D452gHLbQv5hytg",
+        "description": """
+            All students and faculty welcome. This class will help students
+            prepare for the YEI summer fellowship application. Applications
+            due by 1/10 before midnight. Location: Yale SOM 1/14 5-10pm,
+            Room TBD.
+        """,
+        "show": True,
+        "disabled": False,
+    },
